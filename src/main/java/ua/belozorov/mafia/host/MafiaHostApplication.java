d0f617520d10e24c7eebacffff784490ab01e019
Белозьоров Володимир Анатолійович <vbelozorov@priocom.com>
2024-07-17 10:23:23 +0300

    scaffolding

---
+++ src/main/java/ua/belozorov/mafia/host/MafiaHostApplication.java
@@ -0,0 +1,13 @@
+package ua.belozorov.mafia.host;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MafiaHostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MafiaHostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/belozorov/mafia_host/MafiaHostApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.belozorov.mafia_host;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MafiaHostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
